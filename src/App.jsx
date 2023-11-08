1081c03dd27de5a88d10336f58c1bc4dca0c5bea
katiperkio <perkiokati@outlook.com>
2023-11-08 10:47:51 +0200

    close button functional, adding image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import Card from './Card.jsx'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+import { useState } from "react";
+import Card from "./Card.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import img from "./assets/splash.png";
+
+function Greeting({ name }) {
+  if (name === "Kati") {
+    return <p>Welcome {name}</p>;
+  }
+  return <p>Please log in</p>;
+}
 
 function App() {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Kati', title: 'CEO', age: '27' },
-    { id: 2, name: 'Joni', title: 'CEO', age: '29' },
-    { id: 3, name: 'Pasi', title: 'intern', age: '37'}
+    { id: 1, name: "Kati", age: "27" },
+    { id: 2, name: "Joni", age: "29" },
+    { id: 3, name: "Minttu", age: "3" },
+    { id: 4, name: "Marko", age: "4" },
+    { id: 5, name: "Pasi", age: "37" },
   ]);
 
   const removeHandler = (id) => {
@@ -19,15 +29,22 @@ function App() {
     <>
       <Header logo="Kati Perkiö" />
       <main>
+        <Greeting name="Kati" />
         <h2>This is my application</h2>
+        <img className="image" src={img} alt="image" />
         <div className="cards">
-          {persons.map((person) => (<Card key={person.id} {...person} click={removeHandler}/>
+          {persons.map((person) => (
+            <Card
+              key={person.id}
+              {...person}
+              click={() => removeHandler(person.id)}
+            />
           ))}
-      </div>
+        </div>
       </main>
       <Footer />
     </>
-  )
-};
+  );
+}
 
-export default App
+export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,14 @@
-const Card = ({name, age, click}) => {
-    return (
-        <div className="card">
-            <h3>Name: {name}</h3>
-            <p>Age: {age}</p>
-            <button onClick={click}>Remove</button>
-        </div>
-    );
+const Card = ({ name, age, click }) => {
+  return (
+    <div
+      style={age < 30 ? { background: "white" } : { background: "gray" }}
+      className="card"
+    >
+      <h3>Name: {name}</h3>
+      <p className={age < 30 ? "green-age" : "white-age"}>Age: {age}</p>
+      <button onClick={click}>Close</button>
+    </div>
+  );
 };
 
 export default Card;
